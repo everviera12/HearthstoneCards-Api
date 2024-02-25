61fed736e825862f8c7334aa8fcb2687c2867651
EverViera12 <evertigre10@gmail.com>
2024-02-24 18:49:22 -0600

    commit

--- src/components/Acordion.jsx
+++ src/components/Acordion.jsx
@@ -22,7 +22,8 @@ const Acordion = () => {
             <div className="bg-gray-D9D9D9 w-40 h-40 md:w-36  md:h-36 flex items-center justify-center rounded-full overflow-hidden">
               <img
                 className="w-24 h-24 object-cover"
-                src="/src/img/3a1130c99a8f060fcc1557cd2e79e446.png"
+                // src="/src/img/3a1130c99a8f060fcc1557cd2e79e446.png"
+                src="https://s3-alpha-sig.figma.com/img/f5a6/7092/3a1130c99a8f060fcc1557cd2e79e446?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JWd7Q6L29u9CHX0zeB60pwXAOSm2AnHmhX5Mz9XBenEv0sQSb0KWO1i6JVBjGpFTazb-zGGR135AC32pZHb1-pfTm7ck564Ce9CzpNxLBfRPTzdKXXJPJMVulVVW5kbfYlaXZZSKm7Ui7ysPOa~GcC60e3NqWqzNXG5GwjRaGsvr6fjBYp~WsVMcNx0vwjYU6tycbjJKV4NRekvSezxF8UGd9~nY5GsFukl1bSLaXy7TPjzrD12tlTahnEXy2nMzESjvwE9CIKPeK614sDOtMaqWViNYIuQ4D9qZ4WJOL~Y4V-FCA8po~e2ZMC5E5LpGxPKO8sLUjKy2JcLrto70ZA__"
                 alt="user image"
               />
             </div>
@@ -45,7 +46,8 @@ const Acordion = () => {
               Our <span className="text-red-custom"> Commitment</span>
             </span>
             <img
-              src="/src/svg/expand_more_FILL0_wght400_GRAD0_opsz24.svg"
+              // src="/src/svg/expand_more_FILL0_wght400_GRAD0_opsz24.svg"
+              src="https://s3-alpha-sig.figma.com/img/205f/970d/ffd96e54c7dc6543767fbddd3292a154?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PFBeJrcTrAGSP-rg7TfH2WLLXBbWq8qdFVHxi88o~u78U754KKP2bc1nPOOq8cSJsfsurqzepvCd~ymfP0FnQbBLg7-9VNoTtrsujER9HrblnCE7mPzLG5aphFiIlWLZDxA5jk~G1Dv3skFegFvxatkcO1QVkh2GRQHay2zuly5O2wwnuMeRPetGSz2PryhaPn1jeE20bBpxQKAkx2CtwVWgV44XafFUCUPS01RnYiqx4TxTZv25mJGxE8ufr9WEeMlNtrC0u3Y6P18NNAycg1v-CkgvkTgiAHDKYC4NkkSQS0nNI8iXg9VsRhHPh~P3sX6lmfYTONLKAh6UpvQRSg__"
               className="w-5 md:w-7 transition-all duration-500 group-focus:-rotate-180"
               alt="expand icon"
             />
